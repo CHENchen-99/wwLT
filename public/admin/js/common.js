394ebd493e06ee225b7c073683620d1df758c22d
chen <984290403@qq.com>
2019-12-14 12:00:20 +0800

    可以开始做一级分类了

--- public/admin/js/common.js
+++ public/admin/js/common.js
@@ -10,3 +10,38 @@ $(document).ajaxStop(function(){  //ajax开始事件,只要有ajax 发送,就会
     NProgress.done()
   },500)
 })
+
+//给分类管理注册点击事件 二级菜单显示和隐藏
+$('.category').on('click',function(){
+  $('.second').slideToggle()
+})
+
+
+//点击上边三条杠 左边侧边栏显示隐藏
+$('.btn-aside').on('click',function(){
+  $('.aside, .content, .topBar').toggleClass('now')
+})
+
+//退出功能
+$('.btn-logout').on('click', function(){
+  $('.logoutModal').modal('show')
+  // console.log('哈哈哈哈')
+  $('.btn-sure').on('click',function(){
+    $.ajax({
+      type:'get',
+      url:'/employee/employeeLogout',
+      success:function(info){
+        if(info.success){
+          location.href='login.html'
+        }
+      }
+    })
+  })
+})
+
+
+
+
+
+
+
